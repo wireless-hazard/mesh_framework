5cddab184ac22ac1c97cb69a6430113c0b6b6fa3
Magno <magnocostamaia@gmail.com>
2020-05-30 13:56:38 -0300

    handler implemented

--- mesh_framework/main/mesh_framework.c
+++ mesh_framework/main/mesh_framework.c
@@ -18,9 +18,119 @@
 #define MAX_CLIENTS CONFIG_MAX_CLIENTS
  
 static const uint8_t MESH_ID[6] = {0x05, 0x02, 0x96, 0x05, 0x02, 0x96};
+static const char *MESH_TAG = "mesh_tagger";
+static bool is_mesh_connected = false;
+static int mesh_layer = -1;
+static mesh_addr_t mesh_parent_addr;
 
 void mesh_event_handler(mesh_event_t evento){
-	
+	switch (evento.id){
+		static int filhos = 0;
+		static uint8_t last_layer = 0;
+		//
+		//MESH MAIN SETUP EVENTS
+		//
+		case MESH_EVENT_STARTED: //start sending Wifi beacon frames and begin scanning for preferred parent.	
+			is_mesh_connected = false;
+			mesh_layer = esp_mesh_get_layer();
+			ESP_LOGW(MESH_TAG,"MESH STARTED\n");
+
+		break;
+		case MESH_EVENT_STOPPED: //Reset the mesh stack's status on the device
+			is_mesh_connected = false;
+        	mesh_layer = esp_mesh_get_layer();
+			ESP_LOGE(MESH_TAG,"ALGO GRAVE ACONTECEU\n");
+		break;
+		case MESH_EVENT_PARENT_CONNECTED:
+			//COPY AND PAST'ED 
+			mesh_layer = evento.info.connected.self_layer;
+			memcpy(&mesh_parent_addr.addr, evento.info.connected.connected.bssid, 6);
+			last_layer = mesh_layer;
+        	is_mesh_connected = true;
+			if (esp_mesh_is_root()) {
+            	tcpip_adapter_dhcpc_start(TCPIP_ADAPTER_IF_STA);
+        	}
+    		printf("Nao esta finalizado\n");
+		break;
+		case MESH_EVENT_PARENT_DISCONNECTED: //Perform a fixed number of attempts to reconnect before searching for another one
+			is_mesh_connected = false;
+        	mesh_layer = esp_mesh_get_layer();
+			ESP_LOGW(MESH_TAG,"Pai desconectou\n");
+		break;
+		case MESH_EVENT_NO_PARENT_FOUND:
+			ESP_LOGW(MESH_TAG,"eternamente IDLE\nESP32 ira reiniciar\n");
+			esp_restart();
+
+		break;
+		//
+		//MESH NETWORK UPDATE EVENTS
+		//
+		case MESH_EVENT_CHILD_CONNECTED: //A child node sucessfully connects  to the  node's  SoftAP interface
+			filhos = filhos + 1;
+			ESP_LOGW(MESH_TAG,"Mais um filho conectado %d\n",filhos);
+		break;
+		case MESH_EVENT_CHILD_DISCONNECTED: //A child node disconnects from  a node 
+			filhos = filhos - 1;
+			printf(MAC2STR(evento.info.child_disconnected.mac));
+		break;
+		case MESH_EVENT_ROUTING_TABLE_ADD: //A node's descendant (with its possible futher descendants) joins the mesh network
+			ESP_LOGW(MESH_TAG,"MAC adicionado a tabela de roteamento\n");
+		break;
+		case MESH_EVENT_ROUTING_TABLE_REMOVE: //A node's descendant (with its possible futher descendants) desconnects from the mesh network
+			ESP_LOGW(MESH_TAG,"MAC removido da tabela de roteamento\n");
+		break;
+		case MESH_EVENT_ROOT_ADDRESS: //Propagacao do MAC do noh raiz da rede Mesh
+			ESP_LOGW(MESH_TAG,"MESH_EVENT_ROOT_ADDRESS\n");		
+		break;
+		case MESH_EVENT_ROOT_FIXED: //Quando a configuracao de root fixo difere entre dois nohs tentando comunicar
+			ESP_LOGW(MESH_TAG,"MESH_EVENT_ROOT_FIXED ISSO NAO PODE ACONTECER\n");
+		break;
+		case MESH_EVENT_TODS_STATE: //The node is informed of a change in the accessibility of the external DS
+			ESP_LOGW(MESH_TAG,"MESH_EVENT_TODS_STATE\n");
+		break;
+		case MESH_EVENT_VOTE_STARTED: //A new rote election is started in the mesh network
+			ESP_LOGW(MESH_TAG,"Votacao comecou\n");
+		break;
+		case MESH_EVENT_VOTE_STOPPED: //The election has ended
+			ESP_LOGW(MESH_TAG,"Eleicao acabou\n");
+		break;
+		case MESH_EVENT_LAYER_CHANGE: //The node's layer in the mesh network has changed
+			ESP_LOGE(MESH_TAG,"Mudou de Layer\n");
+			mesh_layer = evento.info.layer_change.new_layer;
+			last_layer = mesh_layer;
+		break;
+		case MESH_EVENT_CHANNEL_SWITCH: //The mesh wifi channel has changed
+			ESP_LOGE(MESH_TAG,"Mudou para o canal %d\n",evento.info.channel_switch.channel);
+		break;
+		//
+		//MESH ROOT-SPECIFIC EVENTS
+		//
+		case MESH_EVENT_ROOT_GOT_IP: //The station DHCP client retrieves a dynamic IP configuration or a Static IP is applied
+			ESP_LOGW(MESH_TAG,"MESH_EVENT_ROOT_GOT_IP\n");
+			ESP_LOGI(MESH_TAG,
+                 	 "<MESH_EVENT_ROOT_GOT_IP>sta ip: " IPSTR ", mask: " IPSTR ", gw: " IPSTR,
+                 	 IP2STR(&evento.info.got_ip.ip_info.ip),
+                 	 IP2STR(&evento.info.got_ip.ip_info.netmask),
+                 	 IP2STR(&evento.info.got_ip.ip_info.gw));
+		
+		break;
+		case MESH_EVENT_ROOT_LOST_IP: //The lease time  of the Node's station dynamic IP configuration has expired
+			ESP_LOGE(MESH_TAG,"Root perdeu IP\n");
+		break;
+		case MESH_EVENT_ROOT_SWITCH_REQ: //The root node has received a root switch request from  a candidate root
+			ESP_LOGE(MESH_TAG,"Tao querendo troca de root\n");
+			printf(MAC2STR( evento.info.switch_req.rc_addr.addr));
+		break;
+		case MESH_EVENT_ROOT_ASKED_YIELD: //Another root node with a higher RSSI with the router has asked this root node to yield
+			printf("//TODO \n");
+		break;
+		case MESH_EVENT_SCAN_DONE:
+			ESP_LOGW(MESH_TAG,"MESH_EVENT_SCAN_DONE");
+		break;
+		default:
+		break;
+	}
+
 }
 
 void meshf_init(){
