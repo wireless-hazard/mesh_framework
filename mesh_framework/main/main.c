a74e3ca80139b115ae402c027e5ffc14d5796ed7
Magno Maia <magnocostamaia@gmail.com>
2021-03-29 16:19:32 -0300

    stable v0.1

--- mesh_framework/main/main.c
+++ mesh_framework/main/main.c
@@ -116,7 +116,9 @@ void app_main(void) {
 			int64_t before,after = 0;
 			before = esp_timer_get_time();
 			num_of_wakes = change_sensor_data(num_of_wakes);
-			if((next_sleep_time(2) <= 5)){
+			printf("Faltam %d segundos para a transmissao\n",next_sleep_time(2));
+			if((next_sleep_time(2) <= 10)){
+				meshf_sleep_time(next_sleep_time(2)*1000);
 				use_network(mqtt_data,rx_mensagem);
 			}
 			after = esp_timer_get_time();
